ba85a08addc8e387bcb3677a27bf023c403baf3b
oleksandra <tinkooladik@gmail.com>
2022-02-22 22:36:42 -0500

    compose: added some theming

--- app-compose/src/main/java/com/tinkooladik/tvshows/compose/ui/theme/Theme.kt
+++ app-compose/src/main/java/com/tinkooladik/tvshows/compose/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.tinkooladik.tvshows.compose.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -9,13 +8,17 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Background,
+    surface = Background,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Background,
+    surface = Background,
 
     /* Other default colors to override
     background = Color.White,
@@ -28,7 +31,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun TvShowsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun TvShowsTheme(
+    darkTheme: Boolean = true /* isSystemInDarkTheme() */,
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
